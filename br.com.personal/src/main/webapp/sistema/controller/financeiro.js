a5bd28f5acc5ba8c5fd71d0f586c8550ded48d29
Ulisses <ulisseslourenco@gmail.com>
2017-10-18 07:25:10 -0200

    versão

---
+++ br.com.personal/src/main/webapp/sistema/controller/financeiro.js
@@ -0,0 +1,16 @@
+personal.controller('financeiroController',function($scope, $rootScope,$timeout){
+	
+	$scope.vigente ={};
+	var data = new Date();
+	$scope.mes = [];
+	
+	
+	
+	
+	$scope.vigente = { 'mes' : data.getMonth()};
+	
+	
+	
+	
+	
+});
