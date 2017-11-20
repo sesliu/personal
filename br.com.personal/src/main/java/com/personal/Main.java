ec3d9d1e691c2a7da3f7ff1e3cd5972e2aa1809f
Ulisses <ulisseslourenco@gmail.com>
2017-11-20 18:39:24 -0200

    Update Main.java

--- br.com.personal/src/main/java/com/personal/Main.java
+++ br.com.personal/src/main/java/com/personal/Main.java
@@ -9,18 +9,14 @@ import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 
-public class Main extends SpringBootServletInitializer{
+public class Main{
 		
 	
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
-    }
 	
-		public static void main(String[] args) throws Exception{
+public static void main(String[] args) throws Exception{
 			SpringApplication.run(Main.class, args);
-		}
+}
 		
 		
 		
