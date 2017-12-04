53aff453e084fb5e50b69ededa49b434159b7043
Ulisses <ulisseslourenco@gmail.com>
2017-12-04 20:55:02 -0200

    atualização

---
+++ br.com.personal/src/main/java/com/personal/modelo/RelatorioSaida.java
@@ -0,0 +1,45 @@
+package com.personal.modelo;
+
+public class RelatorioSaida extends Aula {
+	
+	
+	private String valores;
+	private String nomeConcatenado;
+	
+	
+	  private String header;
+	    
+	    
+	    
+	    public String getHeader() {
+			return header;
+		}
+		public void setHeader(String header) {
+			this.header = header;
+		}
+	    
+	
+	
+	
+	
+	
+	
+	public String getNomeConcatenado() {
+		return nomeConcatenado;
+	}
+	public void setNomeConcatenado(String nomeConcatenado) {
+		this.nomeConcatenado = nomeConcatenado;
+	}
+	public String getValores() {
+		return valores;
+	}
+	public void setValores(String valores) {
+		this.valores = valores;
+	}
+	
+	
+	
+	
+	
+
+}
