87f326cc7279fdd41161387cbe757c603e08e420
Ulisses <ulisseslourenco@gmail.com>
2017-12-04 20:55:35 -0200

    atualização

---
+++ br.com.personal/src/main/java/com/personal/modelo/RetornaPDF.java
@@ -0,0 +1,17 @@
+package com.personal.modelo;
+
+public class RetornaPDF {
+
+	String pdf;
+
+	public String getPdf() {
+		return pdf;
+	}
+
+	public void setPdf(String pdf) {
+		this.pdf = pdf;
+	}
+	
+	
+	
+}
