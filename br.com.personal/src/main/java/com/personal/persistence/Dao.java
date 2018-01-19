54ad9ce4fc351f3d536f651aebf2371307e45243
Ulisses <ulisseslourenco@gmail.com>
2018-01-18 23:01:02 -0200

    0.2.4

--- br.com.personal/src/main/java/com/personal/persistence/Dao.java
+++ br.com.personal/src/main/java/com/personal/persistence/Dao.java
@@ -27,7 +27,7 @@ public class Dao {
       //		String user = "b8acf746965ef8";
 	// 	String password = "4260704f";
 		
-		String url = "jdbc:mysql://personalgym.mysql.uhserver.com:3306/personalgym?autoReconnect=true";
+		String url = "jdbc:mysql://personalgym.mysql.uhserver.com:3306/personalgym?connectTimeout=0&socketTimeout=0&autoReconnect=true";
 		String user = "personalgym";
 		String password = "Personal@Gym@22";
 		
