18206ef0845ee350b2c46862bc0d34247cdcb11f
sesliu <ulisseslourenco@gmail.com>
2020-08-10 00:46:46 -0300

    ajustes

--- br.com.personal/src/main/java/com/personal/webservices/AulaWS.java
+++ br.com.personal/src/main/java/com/personal/webservices/AulaWS.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -147,7 +148,7 @@ public class AulaWS {
 	
 	}
 	
-	
+	@CrossOrigin
 	@RequestMapping(value = "/personal", method = RequestMethod.GET)
 	public Personal listarPersonal() {
 
